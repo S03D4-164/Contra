efef8410f489ca0ec5f23b8834370645347f01c1
S03D4-164 <S03D4-164@example.com>
2015-11-23 02:17:54 +0000

    fix for python 3

--- backend/celery.py
+++ backend/celery.py
@@ -8,25 +8,25 @@ from celery import Celery
 from datetime import timedelta
 
 app = Celery(
-	'backend',
-	broker='redis://',
-        backend='redis://',
-        include=[
-		'backend.apis.run_ghost',
-		'backend.apis.dns_resolve',
-		'backend.apis.whois_domain',
-		'backend.apis.whois_ip',
-	])
+    'backend',
+    broker='redis://',
+    backend='redis://',
+    include=[
+        'backend.apis.run_ghost',
+        'backend.apis.dns_resolve',
+        'backend.apis.whois_domain',
+        'backend.apis.whois_ip',
+    ])
 
 app.conf.update(
-	CELERY_DEFAULT_QUEUE = 'backend',
-	CELERY_QUEUES = (
-	    Queue('backend', Exchange('backend'), routing_key='backend'),
-	),
-	#CELERY_IGNORE_RESULT = True,
-	CELERY_TASK_RESULT_EXPIRES = 600,
-	CELERY_MAX_CACHED_RESULTS = -1,
-	CELERYD_TASK_TIME_LIMIT = 600,
+    CELERY_DEFAULT_QUEUE = 'backend',
+    CELERY_QUEUES = (
+        Queue('backend', Exchange('backend'), routing_key='backend'),
+    ),
+    #CELERY_IGNORE_RESULT = True,
+    CELERY_TASK_RESULT_EXPIRES = 600,
+    CELERY_MAX_CACHED_RESULTS = -1,
+    CELERYD_TASK_TIME_LIMIT = 600,
 )
 
 if __name__ == '__main__':
