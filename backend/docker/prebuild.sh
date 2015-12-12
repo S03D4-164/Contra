edc89d0034fef544735321802c8a03c26181ecb5
S03d4-164 <S03d4-164@example.com>
2015-12-12 13:54:26 +0900

    use 4x3 flags

--- backend/docker/prebuild.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/S03D4-164/thug.git && cp -r thug/src/Logging files
-wget https://raw.github.com/vsergeev/u-msgpack-python/master/umsgpack.py && mv umsgpack.py files
-cp -r ../ghost files
